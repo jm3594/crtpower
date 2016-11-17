f096c9b60a39f6ef0098094fe9457c8a10bf5435
Jon Moyer <jon.moyer@gmail.com>
2016-11-17 14:24:55 -0500

    building single power.crt.test function

---
+++ R/power_crt_test.R
@@ -0,0 +1,109 @@
+#' Power calculations for simple cluster randomized trials
+#'
+#' Compute the power of a simple cluster randomized trial, or determine
+#'   parameters to obtain a target power.
+#' @param alpha The level of significance of the test, the probability of a Type I error.
+#' @param power The power of the test, 1 minus the probability of a Type II error.
+#' @param d The standardized effect size.
+#' @param ICC The intra-class correlation.
+#' @param m The number of clusters per arm.
+#' @param n The mean cluster size.
+#' @param type The type of design effect, either standard or based on
+#'   the coefficient of variation.
+#' @param cv The coefficient of variation. Used when 'type' is 'cv'.
+#' @param tol Numerican tolerance used in root finding. The default providing
+#'   (at least) four significant digits.
+#' @return The computed argument.
+
+power.crt.test <- function(alpha = 0.05, power = 0.80,
+                           m = NULL, n = NULL,
+                           d = 0.20, ICC = 0.001,
+                           type = "standard", cv = NULL,
+                           tol = .Machine$double.eps^0.25){
+
+  # check to see that exactly one of the paramaters not specified
+  num_null <- sum(sapply(list(alpha, power, m, n, d, ICC), is.null))
+  if (num_null != 1) {
+    stop("Exactly one of 'alpha', 'power', 'm', 'n', 'd', and 'ICC' must be NULL")
+  }
+
+  # cv value needs to be present if type = cv
+  if (type == "cv" & is.null(cv)) {
+    stop("'cv' must be specified")
+  }
+
+  # function to get design effect
+  getDEFF <- function(type, n, ICC, cv = NULL) {
+    switch(type,
+           "standard" = 1 + (n - 1)*ICC,
+           "cv" = 1 + (((cv^2 + 1)*n) - 1)*ICC)
+  }
+
+  # create call to evaluate power
+  if (is.null(n) | is.null(ICC)) {
+    p.body <- quote({
+      # because n/ICC get updated, DEFF gets updated, so put inside call
+      DEFF <- getDEFF(type, n, ICC, cv)
+      qu <- qt(alpha/2, 2*(m - 1), lower.tail = FALSE)
+      ncp <- sqrt(m*n/(2*DEFF)) * d
+      pt(qu, 2*(m - 1), ncp, lower.tail = FALSE) +
+        pt(-qu, 2*(m - 1), ncp, lower.tail = TRUE)
+    })
+  } else {
+    # DEFF doesn't depend on alpha, power, m, or d, so define outside call
+    DEFF <- getDEFF(type, n, ICC, cv)
+    p.body <- quote({
+      qu <- qt(alpha/2, 2*(m - 1), lower.tail = FALSE)
+      ncp <- sqrt(m*n/(2*DEFF)) * d
+      pt(qu, 2*(m - 1), ncp, lower.tail = FALSE) +
+        pt(-qu, 2*(m - 1), ncp, lower.tail = TRUE)
+    })
+  }
+
+  # calculate alpha
+  if (is.null(alpha)) {
+    alpha <- uniroot(function(alpha) eval(p.body) - power,
+                     interval = c(1e-10, 1 - 1e-10),
+                     tol = tol, extendInt = "yes")$root
+    return(alpha)
+  }
+
+  # calculate power
+  if (is.null(power)) {
+    power <- eval(p.body)
+    return(power)
+  }
+
+  # calculate d
+  if (is.null(d)) {
+    d <- uniroot(function(d) eval(p.body) - power,
+                 interval = c(1e-07, 1e+07),
+                 tol = tol, extendInt = "upX")$root
+    return(d)
+  }
+
+  # calculate m
+  if (is.null(m)) {
+    m <- uniroot(function(m) eval(p.body) - power,
+                 interval = c(2 + 1e-10, 1e+07),
+                 tol = tol, extendInt = "upX")$root
+    return(m)
+  }
+
+  # calculate n
+  if (is.null(n)) {
+    n <- uniroot(function(n) eval(p.body) - power,
+                 interval = c(2 + 1e-10, 1e+07),
+                 tol = tol, extendInt = "upX")$root
+    return(n)
+  }
+
+  # calculate ICC
+  if (is.null(ICC)) {
+    ICC <- uniroot(function(ICC) eval(p.body) - power,
+                   interval = c(1e-7, 1e+07),
+                   tol = tol, extendInt = "downX")$root
+    return(ICC)
+  }
+
+}
